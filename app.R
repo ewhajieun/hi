894864089b965b40695aba564eff5811e1f3c1f2
ewhajieun <cje6479@gmail.com>
2015-10-05 16:14:19 +0900

    Update app.R

--- app.R
+++ app.R
@@ -61,13 +61,13 @@ ui<-
     headerPanel("Asian option Application"),
     
      sidebarPanel(
-       radioButtons("radio",label=h3("Option"),choices=list("Asian Call"=1,"Asian Put"=2),selected=1),
-       sliderInput("r","Interest",min=0,max=1,step=0.01,value=0.5),
+       radioButtons("radio",label=h3("옵션 종류"),choices=list("Asian Call"=1,"Asian Put"=2),selected=1),
+       sliderInput("r","순간 무위험 이자율",min=0,max=1,step=0.01,value=0.5),
       numericInput("sigma","Volatility",0.27),
-      numericInput("s0","Stock price",235600),
+      numericInput("s0","주식 현재 가격",235600),
       
-      numericInput("k","Option Price",235600),
-      numericInput("t","due(year)",1),
+      numericInput("k","옵션 행사 가격",235600),
+      numericInput("t","만기(year)",1),
       
       submitButton("View")
       
@@ -79,7 +79,7 @@ ui<-
   server<-
   shinyServer(function(input,output){
     output$caption<-renderText({
-      "Asian Call & Asian Put"
+      "Asian (average-price) Call & Put option 가격"
     })
     
     output$price<-renderText({
