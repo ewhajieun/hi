72ec75e7765dcd5493f0ec4b0020075a55d9978b
ewhajieun <cje6479@gmail.com>
2015-10-05 16:15:56 +0900

    Update app.R

--- app.R
+++ app.R
@@ -61,7 +61,7 @@ ui<-
     headerPanel("Asian option Application"),
     
      sidebarPanel(
-       radioButtons("radio",label=h3("옵션 종류"),choices=list("Asian Call"=1,"Asian Put"=2),selected=1),
+       radioButtons("radio",label=h3("Option"),choices=list("Asian Call"=1,"Asian Put"=2),selected=1),
        sliderInput("r","순간 무위험 이자율",min=0,max=1,step=0.01,value=0.5),
       numericInput("sigma","Volatility",0.27),
       numericInput("s0","주식 현재 가격",235600),
