7cb9681e0e33d0a106742a867c884fca283f89b4
ewhajieun <cje6479@gmail.com>
2015-11-16 22:11:04 +0900

    Update app.R

--- app.R
+++ app.R
@@ -1,160 +1,234 @@
 
-
-mypremium<-function(kilometres,zone,bonus,make,limit,deductable){
-
-  if(make=="VOLVO V60 N MILJODIESEL Diesel"){make<-1
-  }else if(make=="VOLVO V40N CC MILJODIESE Diesel"){make<-2
-  }else if(make=="VOLVO S60 II MILJODIESEL Diesel"){make<-3
-  }else if(make=="VOLVO V40N MILJODIESEL Diesel"){make<-4
-  }else if(make=="VOLVO XC60 DRIVE-E Diesel"){make<-5
-  }else if(make=="VOLVO V60CC MILJODIESEL"){make<-6
-  }else if(make=="VOLVO XC70II DRIVE-E Diesel"){make<-7
-  }else if(make=="VOLVO V60 N LADDHYBRID Laddhybrid"){make<-8
-  }else if(make=="VOLVO V60CC MILJODIESEL"){make<-9}
-
-  alpha1<-ifelse(kilometres=="less than 1000",1,0)
-  alpha2<-ifelse(kilometres=="from 1000 to 15000",1,0)
-  alpha3<-ifelse(kilometres=="15000 to 20000",1,0)
-  alpha4<-ifelse(kilometres=="20000 to 25000",1,0)
-  alpha5<-ifelse(kilometres=="more than 25000",1,0)
-  
-  beta1<-ifelse(zone=="Stockholm, Goteborg, Malmo with surroundings",1,0)
-  beta2<-ifelse(zone=="Other large cities with surroundings",1,0)
-  beta3<-ifelse(zone=="Smaller cities with surroundings in southern Sweden",1,0)
-  beta4<-0
-  if(zone=="Rural areas in southern Sweden"){beta4<-1}else if(zone=="Gotland"){beta4<-1}
-
-  beta5<-ifelse(zone=="Rural areas in northern Sweden",1,0)
-  beta6<-ifelse(zone=="Smaller cities with surroundings in northern Sweden",1,0)
- 
-  r1<-ifelse(bonus==1,1,0)
-  r2<-ifelse(bonus==2,1,0)
-  r3<-ifelse(bonus==3,1,0)
-  r4<-ifelse(bonus==4,1,0)
-  r5<-ifelse(bonus==5,1,0)
-  r6<-ifelse(bonus==6,1,0)
-  r7<-ifelse(bonus==7,1,0)
-  
-  s2<-ifelse(make==2,1,0)
-  s3<-ifelse(make==3,1,0)
-  s4<-ifelse(make==4,1,0)
-  s5<-ifelse(make==5,1,0)
-  s6<-ifelse(make==6,1,0)
-  s7<-ifelse(make==7,1,0)
-  s1<-0
-  s7<-0
-  if(make==8){s1<-1}else if(make==1){s1<-1}
-  if(make==9){s7<-1}else if(make==9){s7<-1}
-
-
-testdata1<-c(alpha1,alpha2,alpha3,alpha4,beta1,beta2,beta3,beta4,beta5,r1,r2,r3,r4,r5,r6,s1,s2,s3,s4,s5,s6,alpha1*beta4,alpha4*beta4,r1*s1,r1*s5,r2*s1,r2*s5,r3*s1,alpha1*r1,alpha1*r3,alpha2*r1,alpha2*r2,alpha3*r1,alpha3*r6)
-
-
-alpha1_1<-ifelse(kilometres=="less than 1000",1,0)
-  alpha2_1<-ifelse(kilometres=="from 1000 to 15000",1,0)
-  alpha3_1<-ifelse(kilometres=="15000 to 20000",1,0)
-  alpha4_1<-ifelse(kilometres=="20000 to 25000",1,0)
-  alpha5_1<-ifelse(kilometres=="more than 25000",1,0)
-  
-  beta1_1<-ifelse(zone=="Stockholm, Goteborg, Malmo with surroundings",1,0)
-  beta2_1<-ifelse(zone=="Other large cities with surroundings",1,0)
-  beta3_1<-ifelse(zone=="Smaller cities with surroundings in southern Sweden",1,0)
-  beta4_1<-ifelse(zone=="Rural areas in southern Sweden",1,0)
-  beta5_1<-ifelse(zone=="Smaller cities with surroundings in northern Sweden",1,0)
-  beta6_1<-ifelse(zone=="Rural areas in northern Sweden",1,0)
-  beta7_1<-ifelse(zone=="Gotland",1,0)
-  
-  r1_1<-ifelse(bonus==1,1,0)
-  r2_1<-ifelse(bonus==2,1,0)
-  r3_1<-ifelse(bonus==3,1,0)
-  r4_1<-ifelse(bonus==4,1,0)
-  r5_1<-ifelse(bonus==5,1,0)
-  r6_1<-ifelse(bonus==6,1,0)
-  r7_1<-ifelse(bonus==7,1,0)
-  
-  s1_1<-ifelse(make==1,1,0)
-  s2_1<-ifelse(make==2,1,0)
-  s3_1<-ifelse(make==3,1,0)
-  s4_1<-ifelse(make==4,1,0)
-  s5_1<-ifelse(make==5,1,0)
-  s6_1<-ifelse(make==6,1,0)
-  s7_1<-ifelse(make==7,1,0)
-  s8_1<-ifelse(make==8,1,0)
-  s9_1<-ifelse(make==9,1,0)
-
- testdata2<-c(alpha2_1,alpha3_1,alpha4_1,alpha5_1,beta2_1,beta3_1,beta4_1,beta5_1,beta6_1,beta7_1,r2_1,r3_1,r4_1,r5_1,r6_1,r7_1,s2_1,s3_1,s4_1,s5_1,s6_1,s7_1,s8_1,s9_1)
- coef.data1<-c(-2.9177,-0.5704,-0.4144,-0.2873,-0.2068,0.3189,0.0798,-0.0679,-0.2557,-0.2054,
-           1.0697,0.7859,0.6552,0.5067,0.4067,0.3086,0.007,0.1522,-0.1606,-0.5656,0.1640,-0.2619,-0.0684,
-          0.0932,0.2752,0.2436,0.2501,0.3146,0.1379,0.2143,-0.1055,0.3098,0.0905,0.1530,0.1023)
-
- coef.data2<-c(8.476,3.080,-8.337,7.659,-1.237,2.287,4.785,1.287,5.170,1.465,2.278,6.917,4.979,5.119,-7.706,-1.821,-5.207,-3.523,8.435,-1.643,-8.718,-3.932,-1.194,2.135,-5.490 )
-  n.claim<-exp(sum(coef.data1[2:length(coef.data1)]*testdata1)+coef.data1[1])
-  size.claim<-exp(sum(coef.data2[2:length(coef.data2)]*testdata2)+coef.data2[1])
-  
-  if (size.claim<deductable){
-    size.claim1<-0}else if(size.claim>=deductable& size.claim<limit){
-      size.claim1<-size.claim-deductable
-    }else if(size.claim>=limit){
-      size.claim1<-limit-deductable
-    }
-  premium<-c(n.claim*size.claim1,"Krona",n.claim*size.claim1*100/22,"USD")
-  return(premium)
+due.glm.ftn<-function(during.exp,goods.m1,y.i,current.age,
+                      exp.prop,how.long,p.left.date){ # 만기 
+  beta<-c(14.33,-0.04391,0.0361,0.1295,-786.3,1.023,-0.01011,-15.9,-9.033,-1.035,-0.01164,
+          -0.0007603,9.792,-1.036,-0.001723,9.438)
+  goods.m2<-ifelse(goods.m1==2,1,0)
+  goods.m4<-ifelse(goods.m1==4,1,0)
+  goods.m5<-ifelse(goods.m1==5,1,0)
+  x.data<-c(1,during.exp,goods.m2,goods.m4,goods.m5,y.i,current.age,
+                exp.prop,how.long,p.left.date,goods.m2*y.i,goods.m4*y.i,
+                goods.m5*y.i,y.i*exp.prop,y.i*how.long,exp.prop*how.long)
+  #length(beta)
+  #length(x.data)
+  p.value<-pnorm(sum(beta*x.data),mean=0,sd=1)
+  return(p.value)
 }
+
+no.due.glm.ftn<-function(live,y.i,current.age,exp.prop,how.long){ # 무만기 
+  live1<-ifelse(live==1,1,0)
+  beta1<-c(7.15582,0.51521,0.13055,-0.02371,-7.13301,-1.51052)
+  x.data1<-c(1,live1,y.i,current.age,exp.prop,how.long)
+  #length(beta1)
+  #length(x.data1)
+  p.value1<-pnorm(sum(beta1*x.data1),mean=0,sd=1)
+  return(p.value1)
+}
+
+not.due.gam<-function(y.i,current.age,exp.prop,how.long.gam,premium.once){ #만기 
+  library(nlme)
+  library(mgcv)
+  setwd("D://")
+  
+  notmu<-read.table("notmu1.csv",header=T,sep="")
+  colnames(notmu)<-c("Target_Y","age","how.exp","during.exp","how.pay","premium","live","con.date","due.date",
+                     "final.count","goods.m","goods.s","y.i","current.age","exp.prop","how.long","p.left.date","premium.once")
+
+  final.model2.2<-gam(Target_Y ~ y.i + current.age + s(exp.prop) + how.long +
+                        s(premium.once),family=binomial(probit),data=notmu)
+  notmumu<-data.frame(y.i=y.i,current.age=current.age,exp.prop=exp.prop,how.long=how.long.gam,premium.once=premium.once)
+  p<-predict(final.model2.2,newdata=notmumu,type = "response")
+  return(p)
+}
+
+
+due.gam<-function(during.exp,live1,goods.m1,y.i,current.age,exp.prop,how.long.gam,p.left.date){ # 무만기 
+  library(nlme)
+  library(mgcv)
+  setwd("D://")
+  mu<-read.table("mu1.csv",header=T,sep="")
+  colnames(mu)<-c("Target_Y","age","how.exp","during.exp","how.pay","premium","live","con.date","due.date",
+                  "final.count","goods.m","goods.s","y.i","current.age","exp.prop","how.long","p.left.date","premium.once")
+  final.model11<-gam(Target_Y ~ s(during.exp) + live + goods.m + s(y.i) + s(current.age) + s(exp.prop) + 
+                       how.long + p.left.date,family=binomial(logit),data=mu)
+  mumu<-data.frame(during.exp=during.exp,live=live1,goods.m=goods.m1,y.i=y.i,current.age=current.age,
+                   exp.prop=exp.prop,how.long=how.long.gam,p.left.date=p.left.date)
+  mumup<-predict(final.model11,mumu,type = "response")
+  return(mumup)}
+
+cox.ftn<-function(y.i,age,how.long,exp.prop,p.left.date){
+  change.exp<-(exp.prop)^2
+  change.date<-sqrt(p.left.date)
+  beta2<-c(-0.0239438957,-0.43118438776,-9.9925334,9.5183863,0.1514444,
+           -11.9250271638)
+  x.data2<-c(age,how.long,change.exp,change.date,how.long*change.exp,
+            change.exp*change.date)
+  #length(beta2)
+  #length(x.data2)
+  mu.hat<-exp(sum(beta2*x.data2))
+  surv.p<-c(0.999999932560542,0.999999805076187,0.999999585095816,0.99999907810301,
+            0.999996347568668,0.999991939908845,0.999966791890089,
+            0.999956604819783,0.999933248247925,0.999906473101917,
+            0.999886111234325,0.999856773671286,0.999815442740395,
+            0.999776353481332,0.999726046372571,0.999704140634257,
+            0.999671279759543,0.99934316824031,0.998686979440193,
+            0.998150898712199,0.99738491097043,0.996534814181666,
+            0.995364833023809,0.994081425831923,0.993345859026553,
+            0.990802838609986,0.989796916665572,0.988461403261724,
+            0.987239260939469,0.978987423555786,0.952076136307492,
+            0.937470286631671,0.921328624540981,0.903085670232579,
+            0.872893335133969,0.850379491867935,0.827532497644607,
+            0.827532497644607,0.827532497644607,0.752219867849147,
+            0.752219867849147,0.752219867849147,0.752219867849147,
+            0.752219867849147,0.752219867849147,0.752219867849147,
+            0.752219867849147,0.107198466018926,0.0207983871066236,
+            0.000368370153037703,0.000368370153037703,
+            0.00000753958747645294,0.0000000000378079374762143)
+  time<-seq(1,length(surv.p))
+  surv.table<-data.frame(time=time,surv.p=surv.p)
+  if(y.i<=length(surv.p)){
+    surv.value1<-surv.table$surv.p[time==y.i]
+    surv.value2<-surv.table$surv.p[time==(y.i-3)]
+  }else if(length(surv.p)<=y.i & y.i<(length(surv.p)+3)){
+    surv.value1<-0
+    surv.value2<-1
+  }else{
+    surv.value1<-1
+    surv.value2<-1
+  }
+  p.value2<-1-(surv.value1/surv.value2)^mu.hat
+  return(p.value2)
+}
+#p.method<-1
+#age<-30
+#how.exp<-"월납"
+#during.exp<-10
+#live<-"Yes"
+#con.date<-"2001-01-01"
+#final.count<-10
+#goods.m<-"A"
+#due<-TRUE
+#premium<-100000
+myp.value(p.method,age,how.exp,during.exp,live,con.date,
+          final.count,goods.m,due)
+myp.value<-function(p.method,age,how.exp,during.exp,live,con.date,final.count,goods.m,due,premium){
+  if(how.exp=="월납"){how.exp1<-1
+  }else if(how.exp=="3월납"){how.exp1<-2
+  }else if(how.exp=="6월납"){how.exp1<-3
+  }else if(how.exp=="년납"){how.exp1<-4}
+  if(live=="Yes"){
+    live1<-1
+  }else{live1<-0}
+  if(goods.m=="A"){
+    goods.m1<-1
+  }else if(goods.m=="B"){
+    goods.m1<-2
+  }else if(goods.m=="C"){
+    goods.m1<-3
+  }else if(goods.m=="D"){
+    goods.m1<-4
+  }else if(goods.m=="E"){
+    goods.m1<-5
+  }else if(goods.m=="F"){
+    goods.m1<-6
+  }
+  premium.once<-premium*ifelse(how.exp1==1,1,0)+premium*ifelse(how.exp1==2,1,0)/3+
+                premium*ifelse(how.exp1==3,1,0)/6+premium*ifelse(how.exp1==4,1,0)/12
+  y.i<-ifelse(how.exp1==1,1,0)*final.count+ifelse(how.exp1==2,1,0)*final.count*3+
+                 ifelse(how.exp1==3,1,0)*final.count*6+ifelse(how.exp1==4,1,0)*final.count*12
+    current.age<-(2001-as.numeric(substr(con.date,1,4)))+age
+   how.long.gam<-(2001-as.numeric(substr(con.date,1,4))+1)
+   how.long.cox<-(2001-as.numeric(substr(con.date,1,4))+1)*12
+   p.left.date<-y.i/(during.exp*12)
+   
+   today<-as.Date("2001-07-01", format="%Y-%m-%d")	
+   
+     aa<-as.numeric((today-as.Date(con.date, format="%Y-%m-%d"))/365*12)		
+     x9.g<- ifelse(how.exp1==1,ceiling(aa),0)+ifelse(how.exp1==2,ceiling(aa/3),0)+
+            ifelse(how.exp1==3,ceiling(aa/6),0)+ifelse(how.exp1==4,ceiling(aa/12),0)	
+     x9.t<- ifelse(how.exp1==1,ceiling(during.exp*12),0)+ifelse(how.exp1==2,ceiling(during.exp*4),0)+
+             ifelse(how.exp1==3,ceiling(during.exp*2),0)+ifelse(how.exp1==4,during.exp,0)	
+     exp.prop<-ifelse((aa/12)>during.exp,(final.count/x9.t),(final.count/x9.g))		
+     
+     if(p.method==1){
+       if(due==TRUE){
+         pp.value<-due.glm.ftn(during.exp,goods.m1,y.i,current.age,exp.prop,how.long.gam,p.left.date)
+       }else{
+       pp.value<-no.due.glm.ftn(live1,y.i,current.age,exp.prop,how.long.gam)}
+     }else if(p.method==2){
+       if(due==TRUE){
+         pp.value<-not.due.gam(y.i,current.age,exp.prop,how.long.gam,premium.once)
+       }else{
+         
+        pp.value<-due.gam(during.exp,live1,goods.m1,y.i,current.age,exp.prop,how.long.gam,p.left.date)
+       }
+        
+     }else if(p.method==3){
+        pp.value<-cox.ftn(y.i,age,how.long.cox,exp.prop,p.left.date)
+     }
+     return(pp.value)
+}
+
 
 library(shiny)
 
 ui<-
- shinyUI(pageWithSidebar(
-  headerPanel("자동차 보험료 계산"),
-  sidebarPanel(  
-   selectInput(inputId="kilometres",
-               label="주행거리(kilometer)",
-               choices=c("less than 1000","from 1000 to 15000","15000 to 20000","20000 to 25000","more than 25000"),
-               selected="less than 1000"
-               ),
-   selectInput(inputId="zone",
-               label="운전지역(zone)",
-               choices=c("Stockholm, Goteborg, Malmo with surroundings",
-                         "Other large cities with surroundings",
-                         "Smaller cities with surroundings in southern Sweden",
-                         "Rural areas in southern Sweden",
-                         "Smaller cities with surroundings in northern Sweden",
-                         "Rural areas in northern Sweden",
-                         "Gotland"),
-               selected="Stockholm, Goteborg, Malmo with surroundings"),
-   selectInput(inputId="bonus",
-               label="무사고 보너스",
-               choices=seq(1,7),
-               selected=1),
-   selectInput(inputId="make",
-               label="자동차 종류",
-               choices=c("VOLVO V60 N MILJODIESEL Diesel",
-                          "VOLVO V40N CC MILJODIESE Diesel",
-                          "VOLVO S60 II MILJODIESEL Diesel",
-                          "VOLVO V40N MILJODIESEL Diesel",
-                          "VOLVO XC60 DRIVE-E Diesel",
-                          "VOLVO V60CC MILJODIESEL",
-                          "VOLVO XC70II DRIVE-E Diesel",
-                          "VOLVO V60 N LADDHYBRID Laddhybrid",
-                          "VOLVO V60CC MILJODIESEL"),
-               selected=1),
-   numericInput("limit",
-               label="보상한도(limit)",value=10000),
-   numericInput("deductable",
-               label="자기공제액(deductable)",value=0),
-   submitButton("View")
-  ),
- mainPanel(
-  h3(textOutput("caption")),
-  verbatimTextOutput("premium"))
- ))
+shinyUI(pageWithSidebar(
+    headerPanel("보험 해지 확률"),
+    sidebarPanel(  
+      
+      radioButtons(inputId="p.method",
+                  label="방법",
+                  choices=list("GLM"=1,"GAM"=2,"Cox PH"=3),
+                  selected=1),
+      numericInput(inputId="age",
+                   label="가입연령",
+                   value=44),
+      selectInput(inputId="how.exp",
+                  label="납입방법",
+                  choices=list("월납","3월납","6월납","년납"),
+                  selected=1),
+      numericInput(inputId="during.exp",
+                   label="납입기간/년",
+                   value=20),
+      checkboxGroupInput(inputId="live",
+                    label="부활여부",
+                    choices=list("Yes"=1,"No"=2),
+                    selected=2),
+      dateInput(inputId="con.date",
+                label="계약일자",
+                value="1990-02-08"),
+      numericInput(inputId="final.count",
+                   label="최종납입횟수",
+                   value=137),
+      numericInput(inputId="premium",
+                   label="보험료",
+                   value=30000),
+      selectInput(inputId="goods.m",
+                  label="상품중분류",
+                  choices=list("A","B","C","D","E","F"),
+                  selected=1),
+      checkboxInput(inputId="due",
+                    label="만기",
+                    value=TRUE),
+      actionButton("View",label="확인")
+      
+      ),
+    mainPanel(
+      h3(textOutput("caption")),
+      verbatimTextOutput("p.value"))
+  ))
+  
  server<-
  shinyServer(function(input,output){
-   output$caption<-renderText({
-     "자동차 보험료"
-   })
-   output$premium<-renderText({
-    mypremium(input$kilometres,input$zone,input$bonus,input$make,input$limit,input$deductable)
+    
+    output$caption<-renderText({
+      "생명보험 해지확률"
+    })
+    output$p.value<-renderText({
+      myp.value(input$p.method,input$age,input$how.exp,input$during.exp,input$live,input$con.date,
+                input$final.count,input$goods.m,input$due,input$premium)
+    })
   })
-})
+
 shinyApp(ui,server)
